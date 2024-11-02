57939f169ddb0938f46c9299798ead6d37aabaaf
polittraaa <149580306+polittraaa@users.noreply.github.com>
2024-11-02 22:27:32 +0100

    Delete libmx/src/src directory

--- libmx/src/src/is_space.c
+++
@@ -1,6 +0,0 @@
-#include "libmx.h"
-
-bool mx_is_space(char c) {
-    return (c == ' ' || c == '\t' || c == '\n'
-            || c == '\v' || c == '\f' || c == '\r');
-}
--- libmx/src/src/strncmp.c
+++
@@ -1,9 +0,0 @@
-#include "libmx.h"
-
-int mx_strncmp(const char *s1, const char *s2, int n) {
-    for (int i = 0; i < n; i++) {
-        if (s1[i] != s2[i] || s1[i] == '\0')
-            return s1[i] - s2[i];
-    }
-    return 0;
-}
